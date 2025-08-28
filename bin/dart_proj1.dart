be7a9b10972d75e2ddfd600f03d0b4092b0f8205
Araya <6631501125@lamduan.mfu.ac.th>
2025-08-28 18:37:53 +0700

    initial commit

---
+++ bin/dart_proj1.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() {
+  
+}
---
+++ test/dart_proj1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_proj1/dart_proj1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
