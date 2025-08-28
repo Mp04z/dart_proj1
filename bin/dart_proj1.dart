594c213aa46b9c7a4324f9651d3d8cdef7596b8d
Araya <6631501125@lamduan.mfu.ac.th>
2025-08-28 20:52:22 +0700

    final login, all

--- bin/dart_proj1.dart
+++ bin/dart_proj1.dart
@@ -1,15 +1,27 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
+const String baseUrl = "http://localhost:3000";
+String? loggedInUserId;
+String? loggedInUsername;
 
 void main() async {
+    await login(); // ให้ล็อกอินก่อน
+
+  if (loggedInUserId == null) {
+    print("Login failed. Exiting...");
+    return;
+  }
+
   while (true) {
-    showmenu();
     final choice = stdin.readLineSync();
 
     switch (choice) {
       case '1':
         await showall();
+        break;
+      case '2':
+        await showtoday();
         break;
       case '6':
         print("Goodbye!");
@@ -20,39 +32,6 @@ void main() async {
   }
 }
 
-void showmenu() {
-  print("\n========= Expense Tracking App =========");
-  print("Welcome Tom");
-  print("1. All expenses");
-  print("2. Today's expense");
-  print("3. Search expense");
-  print("4. Add new expense");
-  print("5. Delete an expense");
-  print("6. Exit");
-  stdout.write("Choose...");
-}
-
-Future<void> showall() async {
-  final uri = Uri.parse('http://localhost:3000/expenses');
-  final response = await http.get(uri);
-
-  if (response.statusCode == 200) {
-    final List result = jsonDecode(response.body);
-
-    int total = 0;
-    print("\n------------ All expenses -----------");
-    for (final exp in result) {
-      print('${exp["id"]}. ${exp["item"]} : ${exp["paid"]}฿ : ${exp["date"]}');
-      total += exp["paid"] as int;
-    }
-    print("Total expenses = ${total}฿");
-  } else {
-    print("Error: ${response.statusCode}");
-    print("Connection error!");
-  }
-}
-const String baseUrl = "http://localhost:3000";
-String? loggedInUserId;
 
 Future<void> login() async {
   print("===== Login =====");
@@ -75,12 +54,48 @@ Future<void> login() async {
     final result = jsonDecode(response.body);
     print(" ${result['message']}");
     loggedInUserId = result['userId'].toString();
-    showtoday();
+    loggedInUsername = username;
+    showmenu();
   } else {
     final result = jsonDecode(response.body);
     print(" ${result['message']}");
   }
 }
+void showmenu() {
+  
+  print("\n========= Expense Tracking App =========");
+  print("Welcome ${loggedInUsername ?? 'Guest'}");
+  print("1. All expenses");
+  print("2. Today's expense");
+  print("3. Search expense");
+  print("4. Add new expense");
+  print("5. Delete an expense");
+  print("6. Exit");
+  stdout.write("Choose...");
+}
+
+Future<void> showall() async {
+final uri = Uri.parse('$baseUrl/expenses?userId=$loggedInUserId');
+final response = await http.get(uri);
+
+
+  if (response.statusCode == 200) {
+    final List result = jsonDecode(response.body);
+
+    int total = 0;
+    print("\n------------ All expenses -----------");
+    for (final exp in result) {
+      print('${exp["id"]}. ${exp["item"]} : ${exp["paid"]}฿ : ${exp["date"]}');
+      total += exp["paid"] as int;
+    }
+    print("Total expenses = ${total}฿");
+    showmenu();
+  } else {
+    print("Error: ${response.statusCode}");
+    print("Connection error!");
+  }
+}
+
 
 Future<void> showtoday() async {
   final url = Uri.parse('$baseUrl/expenses/today/$loggedInUserId');
